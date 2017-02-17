f96ab8c8ec4a74e04e713b8e01277a5bd441e088
sumnima <sumneerijal@gmail.com>
2017-02-17 13:01:52 +0545

    calculator is added

---
+++ calculator.py
@@ -0,0 +1,14 @@
+num1=input("enter the valueof num1:")
+num2=input("enter the value of num2:")
+add=num1+num2
+sub=num2-num1
+mul=num1*num2
+div=num2/num1
+mod=num2%num1
+print(add)
+print(sub)
+print(mul)
+print(div)
+print(mod)
+
+
